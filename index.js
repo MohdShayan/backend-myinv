7ed3bcc5f15d04807a4195234fac7fcb24ab85fb
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-11-30 09:49:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { ConnectDB } from "./config/db.js ";
+import { ConnectDB } from "./config/db.js";
 import cors from 'cors';
 
 import productRoutes from "./routes/productRoutes.js"  
